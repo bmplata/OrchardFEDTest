f6d2662d71b83a7b25a29be3daba1a360284cb72
bmp <burneymagsino@gmail.com>
2024-03-25 12:37:42 +0800

    Project Setup
    Added repository structure and configs
    
    Signed-of-by: Burney Plata

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        filename: 'bundle.js', // Output bundle filename
+        publicPath: '/', // Public path for assets served by webpack-dev-server
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // Apply CSS loader for .css files
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|ttf)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets', // Output path for images in the dist directory
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // HTML template file
+            filename: 'index.html', // Output HTML filename
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'), // Serve files from the dist directory
+        },
+        compress: true, // Enable gzip compression
+        port: 9000, // Port for webpack-dev-server
+        open: true, // Automatically open the default browser
+    },
+};
